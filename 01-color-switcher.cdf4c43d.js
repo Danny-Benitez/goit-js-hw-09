843bd93f33a2561eb56745ae4248992d2b4043ba
Danny-Benitez <76267992+Danny-Benitez@users.noreply.github.com>
2023-08-23 17:13:34 +0000

    Deploying to gh-pages from @ Danny-Benitez/goit-js-hw-09@8f67d305c84427c08d08ba125ba58527c48c55cc 🚀

---
+++ 01-color-switcher.cdf4c43d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let d;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t,d=setTimeout(o,1e3)}t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,o()})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearTimeout(d)}));
+//# sourceMappingURL=01-color-switcher.cdf4c43d.js.map
